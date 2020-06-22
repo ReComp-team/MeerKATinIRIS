927f670f6fbb9712edf0a71af946641170a3952a
priyaa-t <59660575+priyaa-t@users.noreply.github.com>
2020-06-22 20:48:25 +0100

    Setup file to create myconfig.txt file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#For cshrc
+set DIR = `dirname "$BASH_SOURCE/processMeerKAT"`
+set PATH = {$PATH}:$DIR
+set PYTHONPATH = {$PYTHONPATH}:$DIR
+
+#For bashrc
+#dir=$(dirname $BASH_SOURCE)/processMeerKAT
+#export PATH=$PATH:$dir
+#export PYTHONPATH=$PYTHONPATH:$dir
